c63009b55094eae7c1a87421a71f9b47cd8ea25e
Austin Cumberlander <austincumberlander@gmail.com>
2020-10-23 13:57:58 -0500

    scholarhship, partner, and app page ui is complete.

--- src/pages/ProgramsPage/ProgramsPage.js
+++ src/pages/ProgramsPage/ProgramsPage.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import MyFooter from '../../components/MyFooter/MyFooter';
 import './ProgramsPage.scss';
-import { Accordion, Card } from 'reactstrap';
 import BootcampDropdown from '../../components/BootcampDropdown/BootcampDropdown';
 
 function ProgramsPage() {
@@ -60,62 +59,57 @@ function ProgramsPage() {
 	});
 
 	return (
-		<div>
-			<>
-				<div className="programs-page-container">
-					<div class="card programs-card-container mt-5">
-						<div class="card-body programs-card">
-							<h1 className="programs-title text-center text-white">
-								Programs
-							</h1>
-						</div>
+		<>
+			<div className="programs-page-container">
+				<div className="card programs-card-container mt-5">
+					<div className="card-body programs-card">
+						<h1 className="programs-title text-center text-white">Programs</h1>
 					</div>
+				</div>
 
-					<div class="grid-container">
-						<div class="card programs-text-card mt-5">
-							<div class="card-body programs-text-card">
-								<p className="bold-text">Accelerate Part Time Courses</p>
-								<p>
-									Pivot Tech offers accelerated part time courses for the fields
-									of data analytics, web development, and soon cyber security.
-									This program option was made with the working professional in
-									mind, by providing a flexible and evening based course
-									schedule.
-								</p>
-								<p className="bold-text">Schedule</p>
+				<div className="grid-container">
+					<div className="card programs-text-card mt-5">
+						<div className="card-body programs-text-card">
+							<p className="bold-text">Accelerate Part Time Courses</p>
+							<p>
+								Pivot Tech offers accelerated part time courses for the fields
+								of data analytics, web development, and soon cyber security.
+								This program option was made with the working professional in
+								mind, by providing a flexible and evening based course schedule.
+							</p>
+							<p className="bold-text">Schedule</p>
 
-								<p>Two Evenings: 3 hours of instruction</p>
-								<div class="same-line">
-									<p>Saturday: 3 hours of group study </p>
-									<i>(instructor will be available during lab hours)</i>
-								</div>
+							<p>Two Evenings: 3 hours of instruction</p>
+							<div className="same-line">
+								<p>Saturday: 3 hours of group study </p>
+								<i>(instructor will be available during lab hours)</i>
 							</div>
 						</div>
 					</div>
-					<div class="outer-card"></div>
-					<BootcampDropdown key={dataAnalytics.id} program={dataAnalytics} />
-					<BootcampDropdown key={webDevelopment.id} program={webDevelopment} />
-					<BootcampDropdown key={cyberSecurity.id} program={cyberSecurity} />
-
-					<div class="card programs-text-card mt-5 mb-5">
-						<div class="card-body programs-text-card">
-							<p className="bold-text">Individual Course Offerings</p>
-							<p>
-								Pivot Tech offers individual courses for the field of data
-								analytics. These courses consist of multilple videos that will
-								allow the user to easily follow along and gain a new skillset in
-								the process.
-							</p>
-						</div>
-					</div>
-					<BootcampDropdown
-						key={individualCourse.id}
-						program={individualCourse}
-					/>
 				</div>
-				<MyFooter />
-			</>
-		</div>
+				<div className="outer-card"></div>
+				<BootcampDropdown key={dataAnalytics.id} program={dataAnalytics} />
+				<BootcampDropdown key={webDevelopment.id} program={webDevelopment} />
+				<BootcampDropdown key={cyberSecurity.id} program={cyberSecurity} />
+
+				<div className="card programs-text-card mt-5 mb-5">
+					<div className="card-body programs-text-card">
+						<p className="bold-text">Individual Course Offerings</p>
+						<p>
+							Pivot Tech offers individual courses for the field of data
+							analytics. These courses consist of multilple videos that will
+							allow the user to easily follow along and gain a new skillset in
+							the process.
+						</p>
+					</div>
+				</div>
+				<BootcampDropdown
+					key={individualCourse.id}
+					program={individualCourse}
+				/>
+			</div>
+			<MyFooter />
+		</>
 	);
 }
 
